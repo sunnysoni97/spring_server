1b339d5b0691e34fd4d6740608ad88cf8ac7f97a
Sunny Soni <sunnysoni97@gmail.com>
2019-07-10 17:50:03 +0530

    initial image uploader

--- src/main/java/com/main/app/server_app/controllers/formController.java
+++ src/main/java/com/main/app/server_app/controllers/formController.java
@@ -1,6 +1,9 @@
 package com.main.app.server_app.controllers;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -17,54 +20,76 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @SessionAttributes("user")
-public class formController{
-    
-    @RequestMapping(value="/new_user", method={RequestMethod.GET})
-    public String serveForm(Model model, @RequestParam(name="entry_code", required=false, defaultValue="0") int entryCode){
+public class formController {
+
+    @RequestMapping(value = "/new_user", method = { RequestMethod.GET })
+    public String serveForm(Model model,
+            @RequestParam(name = "entry_code", required = false, defaultValue = "0") int entryCode) {
         consoleLogger.info("Entered into formController - serveForm()");
         consoleLogger.info("serveForm entry code = " + entryCode);
         model.addAttribute("entryStatus", new EntryStatus(entryCode));
         model.addAttribute("user", new User());
         return "user_reg_form";
     }
-    
-    @RequestMapping(value="/register_details", method={RequestMethod.GET})
-    public String serveDetailsForm (@ModelAttribute User user){
+
+    @RequestMapping(value = "/register_details", method = { RequestMethod.GET })
+    public String serveDetailsForm(@ModelAttribute User user) {
         consoleLogger.info("Entered into formController - serveDetailsForm()");
         consoleLogger.info("Username - " + user.getUserName());
         return "user_det_form";
+    }
+
+    @RequestMapping(value = "/view_details", method = { RequestMethod.GET })
+    public String serveDetails(@ModelAttribute User user) {
+        consoleLogger.info("Entered into formController - serveDetails()");
+        return "view_details_page";
     }
 
 }
 
 @Controller
 @SessionAttributes("user")
-class formControllerPosts{
-    
-    @RequestMapping(value="/logout", method={RequestMethod.POST})
-    public void logout(HttpServletResponse httpServletResponse, SessionStatus sessionStatus) throws IOException
-    {
+class formControllerPosts {
+
+    @RequestMapping(value = "/logout", method = { RequestMethod.POST })
+    public void logout(HttpServletResponse httpServletResponse, SessionStatus sessionStatus) throws IOException {
         consoleLogger.info("Entered into formControllerPosts - logout()");
         sessionStatus.setComplete();
         httpServletResponse.sendRedirect("");
     }
-   
-    @RequestMapping(value="/new_user", method={RequestMethod.POST})
-    public void serveResults(@ModelAttribute User user, HttpServletResponse httpServletResponse, SessionStatus sessionStatus) throws IOException{
+
+    @RequestMapping(value = "/new_user", method = { RequestMethod.POST })
+    public void serveResults(@ModelAttribute User user, HttpServletResponse httpServletResponse,
+            SessionStatus sessionStatus) throws IOException {
         consoleLogger.info("Entered into formControllerPosts - serveResults()");
         ValidateNewUser vnu = new ValidateNewUser();
         int flag = vnu.checkData(user);
-        if (flag==0){
+        if (flag == 0) {
             httpServletResponse.sendRedirect("register_details");
-        }
-        else
-        {
+        } else {
             sessionStatus.setComplete();
-            httpServletResponse.sendRedirect("new_user?entry_code="+flag);
+            httpServletResponse.sendRedirect("new_user?entry_code=" + flag);
+        }
+    }
+
+    @RequestMapping(value = "/upload_file", method = { RequestMethod.POST })
+    public void uploadFile(@RequestParam("file") MultipartFile file, HttpServletResponse httpServletResponse, @ModelAttribute User user) throws IOException {
+        consoleLogger.info("Entered into formControllerPosts - uploadFile()");
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get("images/" + user.getUserName() + ".png");
+                Files.write(path.toAbsolutePath(), bytes);
+                consoleLogger.info("Image Successfully uploaded!");
+                httpServletResponse.sendRedirect("view_details");
+            } catch (IOException e){
+                consoleLogger.errorMsg("error while file uploading!");
+                e.printStackTrace();
+            }
         }
     }
 
